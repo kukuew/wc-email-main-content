987844728e7b4af03366a649ddcc106c0ffd238c
mikolaj360px <mykola@360px.pl>
2021-04-06 21:44:04 +0200

    init

---
+++ wc-email-main-content.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Email Main Content
+ * Description: Allow to set main content in woocommerce emails.
+ * Plugin URI:  https://github.com/kukuew/wc-email-main-content
+ * Author URI:  https://github.com/kukuew
+ * Author:      Mykola Smuzhanytsia
+ *
+ * Text Domain: wc-email-main-content
+ * Domain Path: /languages/
+ *
+ * Requires PHP: 7.4
+ * Requires at least: 5.3
+ *
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Version:     1.0.0
+ *
+ */
